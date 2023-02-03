7fdb98f9c8ea6cc4e7e52e6f7cba80906e85de24
asiyecan <asiyecan.pdr@gmail.com>
2023-02-03 19:36:28 +0300

    Cucumber

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -1,4 +1,17 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+        features = "./src/test/resources/features",
+        dryRun = false,
+        glue = "stepdefinitions",
+        tags = "@UITest"
+)
+
 public class Runner {
 }
